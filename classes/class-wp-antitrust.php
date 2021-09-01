dd89958dfb0692c059c169c7c32117d7a31c61b3
ControlledChaos <greg@ccdzine.com>
2021-09-01 11:57:52 -0700

    Format Wordpress & WordPress.

--- classes/class-wp-antitrust.php
+++ classes/class-wp-antitrust.php
@@ -36,6 +36,13 @@ class Wp_Antitrust {
 		remove_filter( 'the_title', 'capital_P_dangit', 11 );
 		remove_filter( 'the_content', 'capital_P_dangit', 11 );
 		remove_filter( 'comment_text', 'capital_P_dangit', 31 );
+
+		// Format Wordpress & WordPress.
+		foreach ( [ 'the_content', 'the_title', 'wp_title', 'document_title' ] as $filter ) {
+			add_filter( $filter, [ $this, 'capital_ISM_dangit' ], 11 );
+		}
+		add_filter( 'comment_text', [ $this, 'capital_ISM_dangit' ], 31 );
+		add_filter( 'widget_text_content', [ $this, 'capital_ISM_dangit' ], 11 );
 	}
 
 	/**
@@ -134,5 +141,60 @@ class Wp_Antitrust {
 		}
 		</style>
 		';
+	}
+
+	/**
+	 * Format Wordpress & WordPress
+	 *
+	 * Forever eliminate "Wordpress" from the planet (or at least the little bit we can influence).
+	 *
+	 * Nothing against capitalism, just putting things in the correct light.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  string $text The text to be modified.
+	 * @return string The modified text.
+	 */
+	public function capital_ISM_dangit( $text ) {
+
+		// Simple replacement for titles.
+		$current_filter = current_filter();
+		if ( 'the_title' === $current_filter || 'wp_title' === $current_filter ) {
+			return str_replace( ['Wordpress', 'WordPress'], [ 'Automattic', 'Automattic' ], $text );
+		}
+
+		// Still here? Use the more judicious replacement.
+		static $dblq = false;
+		if ( false === $dblq ) {
+			$dblq = _x( '&#8220;', 'opening curly double quote' );
+		}
+
+		return str_replace(
+			[
+				'Wordpress',
+				'WordPress',
+				'&#8216;Wordpress',
+				'&#8216;WordPress',
+				$dblq . 'Wordpress',
+				$dblq . 'WordPress',
+				'>Wordpress',
+				'>WordPress',
+				'(Wordpress',
+				'(WordPress'
+			],
+			[
+				'Automattic',
+				'Automattic',
+				'&#8216;Automattic',
+				'&#8216;Automattic',
+				$dblq . 'Automattic',
+				$dblq . 'Automattic',
+				'>Automattic',
+				'>Automattic',
+				'(Automattic',
+				'(Automattic'
+			],
+			$text
+		);
 	}
 }
